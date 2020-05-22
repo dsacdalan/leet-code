cf44f11c836c3c712075c8bd2248513156610c4b
Daniel Sacdalan <cheeseter550@gmail.com>
2020-05-22 18:12:26 -0400

    Add Grumpy

--- LeetCode.Service/GrumpyOwner.cs
+++ LeetCode.Service/GrumpyOwner.cs
@@ -12,32 +12,35 @@ namespace LeetCode.Service
 
             // get first window
             int max = 0;
-            int maxIndex = 0;
+            int startIndex = 0;
             for (int i = 0; i < X; i++) {
                 if (grumpy[i] == 1)
                     max += customers[i];
             }
+            var current = max;
 
             for (int i = X; i < customers.Length; i++) {
-                var current = max;
-                if (grumpy[i] == 1)
+                // If current is grumpy
+                if (grumpy[i] == 1) {
                     current += customers[i];
-                if (grumpy[i - X] == 1)
-                    current -= customers [i - X];
+                }
+                var currentStart = i - X;
+                if (grumpy[currentStart] == 1) {
+                    current -= customers[currentStart];
+                }
                 if (current > max) {
                     max = current;
-                    maxIndex = i - X + 1;
-                }               
+                    startIndex = currentStart + 1;
+                }
             }
 
             var answer = 0;
             for (int i = 0; i < customers.Length; i++) {
-                if (i < maxIndex || i > maxIndex + X) {
-                    if (grumpy[i] == 0) {
-                        answer += customers[i];
-                    }
+                if (grumpy[i] == 0) {
+                    answer += customers[i];
                 }
-                else {
+                else if (i >= startIndex && i <= startIndex + X - 1) 
+                {
                     answer += customers[i];
                 }
             }
--- LeetCode.Test/GrumpyTests.cs
+++ LeetCode.Test/GrumpyTests.cs
@@ -25,7 +25,7 @@ namespace LeetCode.Test
             Assert.AreEqual(16, result);
         }
 
-                [TestMethod]
+        [TestMethod]
         public void Grumpy_2()
         {
             var customers = new int[] {9,10,4,5};
@@ -35,6 +35,30 @@ namespace LeetCode.Test
             var result = _grumpy.MaxSatisfied(customers, grumpy, X);
 
             Assert.AreEqual(19, result);
+        }
+
+        [TestMethod]
+        public void Grumpy_3()
+        {
+            var customers = new int[] {3,8,8,7,1};
+            var grumpy = new int[] {1,1,1,1,1};
+            var X = 3;
+
+            var result = _grumpy.MaxSatisfied(customers, grumpy, X);
+
+            Assert.AreEqual(23, result);
+        }
+
+        [TestMethod]
+        public void Grumpy_4()
+        {
+            var customers = new int[] {7,8,8,6};
+            var grumpy = new int[] {0,1,0,1};
+            var X = 3;
+
+            var result = _grumpy.MaxSatisfied(customers, grumpy, X);
+
+            Assert.AreEqual(29, result);
         }
     }
 }
