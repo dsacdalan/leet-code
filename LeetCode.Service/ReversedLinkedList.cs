a08f0cb465b933adc4a18955b61838c2357269ee
Daniel Sacdalan <cheeseter550@gmail.com>
2020-05-26 18:53:29 -0400

    Add ReverseList

---
+++ LeetCode.Service/ReversedLinkedList.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+
+namespace LeetCode.Service
+{
+    public class ReversedLinkedList
+    {
+        private ListNode Helper(ListNode head, ListNode reverse)
+        {
+            if (head == null)
+                return reverse;
+
+            var next = head.next;
+            head.next = reverse;
+            return Helper(next, head);
+        }
+
+        public ListNode ReverseList(ListNode head)
+        {
+            return Helper(head, null);
+        }
+
+        public ListNode ReverseList_Iterative(ListNode head)
+        {
+            ListNode reverse = null;
+
+            while(head != null)
+            {
+                var next = head.next;
+                head.next = reverse;
+                reverse = head;
+                head = next;
+            }
+
+            return reverse;
+        }
+    }
+}
